79263cc56c4d2f83efa9b05a938058fcd6e72cee
Juan Lipez <juandavidlipez47@gmail.com>
2024-03-04 11:52:52 -0500

    🔨cached user information to avoid multiple requests to supabase

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,14 +1,32 @@
+import { map } from "nanostores";
 import { supabase } from "./supabase";
 import type { AstroCookies } from "astro";
+import type { Session, User } from "@supabase/supabase-js";
+
+interface AuthInterface {
+  userData: {
+    user: User | null
+    session: Session | null
+  } | null
+}
 
 export class Auth {
+
+  private static auth = map<AuthInterface>({
+    userData: null,
+  });
 
   static async getUserData(cookies: AstroCookies) {
     const accessToken = cookies.get("sb-access-token");
     const refreshToken = cookies.get("sb-refresh-token");
 
     if (!accessToken || !refreshToken) {
+      this.auth.set({
+        userData: null,
+      });
       return false;
+    } else if (this.auth.get().userData) {
+      return this.auth.get().userData;
     }
 
     const { data, error } = await supabase.auth.setSession({
@@ -23,10 +41,12 @@ export class Auth {
       cookies.delete("sb-refresh-token", {
         path: "/",
       });
-
-      return false;
+      return null;
     }
 
+    this.auth.set({
+      userData: data,
+    });
     return data;
   }
 
