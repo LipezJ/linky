a84d45bab6570b0a0f56f8edd9c0c98a6d2599d0
Juan Lipez <juandavidlipez47@gmail.com>
2024-03-27 14:48:01 -0500

    ⚙️change request link from supabase

--- src/db/links.ts
+++ src/db/links.ts
@@ -3,12 +3,14 @@ import { supabase } from "@/lib/supabase";
 export const createLink = async (link: string) => {
 
   const code =  crypto.randomUUID().split("-")[0];
+  const { data } = await supabase.auth.getUser();
 
   const { error } = await supabase
-    .from("link_temp").insert([
+    .from("links").insert([
       {
         link,
         link_short_code: code,
+        user: data.user?.id ?? null,
       },
     ]);
 
@@ -19,24 +21,11 @@ export const createLink = async (link: string) => {
 };
 
 export const getLink = async (code: string) => {
-
   const { data, error } = await supabase
-    .from("link_temp")
-    .select("id,link,uses")
-    .eq("link_short_code", code);
+    .rpc("get_link_by_code", { code })
 
-  if (error || data.length === 0)
-    return null
-  
-  if (data[0].uses <= 5) {
-    await supabase.from("link_temp")
-      .update({ uses: data[0].uses + 1 })
-      .eq("id", data[0].id);
-  } else {
-    await supabase.from("link_temp")
-      .delete()
-      .eq("id", data[0].id);
-  }
+  if (error || data.length === 0 || data == "None")
+    return null;
 
-  return data[0].link;
+  return data;
 };
