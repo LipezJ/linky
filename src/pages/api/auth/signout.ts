76f545234facd3800ad9ef7025c5d73e06b81252
Juan Lipez <juandavidlipez47@gmail.com>
2024-03-27 11:06:11 -0500

    🪛add call to singout method using supabase client

--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,7 +1,11 @@
 import type { APIRoute } from "astro";
+import { supabase } from "@/lib/supabase";
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
   cookies.delete("sb-access-token", { path: "/" });
   cookies.delete("sb-refresh-token", { path: "/" });
+
+  supabase.auth.signOut();
+
   return redirect("/");
 };
