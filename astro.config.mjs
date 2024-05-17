2dc8f5034d41ac933841d9aeb49b771cd9ef5305
Juan Lipez <juandavidlipez47@gmail.com>
2024-05-17 14:09:09 -0500

    🔨format config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import preact from "@astrojs/preact"
 
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact({
-    compat: true
-  })],
-  output: "server",
-  adapter: netlify()
-});
+	integrations: [
+		tailwind(),
+		preact({
+			compat: true,
+		}),
+	],
+	output: "server",
+	adapter: netlify(),
+})
